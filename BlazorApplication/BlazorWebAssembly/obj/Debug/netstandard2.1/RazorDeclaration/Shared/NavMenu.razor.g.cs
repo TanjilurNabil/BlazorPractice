886b5d2ec5747297c7784ca63ce946239abea632
TanjilurNabil <nabil.ajkerdeal@gmail.com>
2022-01-06 17:59:39 +0600

    TODO app implemented

--- BlazorApplication/BlazorWebAssembly/obj/Debug/netstandard2.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ BlazorApplication/BlazorWebAssembly/obj/Debug/netstandard2.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,63 +13,63 @@ namespace BlazorWebAssembly.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 1 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 2 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 3 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 4 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 5 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 6 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 7 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 8 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using BlazorWebAssembly;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\_Imports.razor"
+#line 9 "D:\Blazor\BlazorApplication\BlazorWebAssembly\_Imports.razor"
 using BlazorWebAssembly.Shared;
 
 #line default
@@ -83,7 +83,7 @@ using BlazorWebAssembly.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "H:\Practice project\BlazorPractice\BlazorApplication\BlazorWebAssembly\Shared\NavMenu.razor"
+#line 33 "D:\Blazor\BlazorApplication\BlazorWebAssembly\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
